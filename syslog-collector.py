2b682946dbaccd956e7e3c4b6cad70707dd02fcd
Andrey Semakin <and-semakin@ya.ru>
2017-05-18 15:03:50 +0500

    Add syslog collector

--- syslog-collector.py
+++ syslog-collector.py
@@ -7,7 +7,7 @@
 ## That's it... it does nothing else...
 ## There are a few configuration parameters.
 
-LOG_FILE = 'youlogfile.log'
+LOG_FILE = 'logs/collector_errors.log'
 HOST, PORT = "0.0.0.0", 514
 
 #
@@ -16,22 +16,32 @@ HOST, PORT = "0.0.0.0", 514
 
 import logging
 import socketserver
+from elasticsearch import Elasticsearch
+from datetime import datetime
+
+es = Elasticsearch()
 
 logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='', filename=LOG_FILE, filemode='a')
 
+def normalize(raw):
+    return {'raw': raw}
+
 class SyslogUDPHandler(socketserver.BaseRequestHandler):
 
-	def handle(self):
-		data = bytes.decode(self.request[0].strip())
-		socket = self.request[1]
-		print( "%s : " % self.client_address[0], str(data))
-		logging.info(str(data))
+    def handle(self):
+        today = datetime.now().strftime('%Y-%m-%d')
+        data = bytes.decode(self.request[0].strip())
+        socket = self.request[1]
+        print( "%s : " % self.client_address[0], str(data))
+        result = es.index(index=today, doc_type='event', body=normalize(str(data)))
+        if not result['created']:
+            logging.info(str(data))
 
 if __name__ == "__main__":
-	try:
-		server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
-		server.serve_forever(poll_interval=0.5)
-	except (IOError, SystemExit):
-		raise
-	except KeyboardInterrupt:
-		print ("Crtl+C Pressed. Shutting down.")
+    try:
+        server = socketserver.UDPServer((HOST,PORT), SyslogUDPHandler)
+        server.serve_forever(poll_interval=0.5)
+    except (IOError, SystemExit):
+        raise
+    except KeyboardInterrupt:
+        print ("Crtl+C Pressed. Shutting down.")
