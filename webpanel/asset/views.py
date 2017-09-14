df659a2f05fa6efb9ff747f7e3b7632bcd275994
Andrey Semakin <and-semakin@ya.ru>
2017-09-15 01:31:25 +0500

    Add new view for AlertList

--- webpanel/asset/views.py
+++ webpanel/asset/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import Asset, OperatingSystem
+from django.shortcuts import render, get_object_or_404
+from .models import Asset, OperatingSystem, Alert, AlertType
 from django.contrib.auth.decorators import login_required
+
 
 # Страница с активами
 @login_required
@@ -11,7 +12,7 @@ def AssetList(request, operation_system=None):
     if operation_system is not None:
         os = get_object_or_404(OperatingSystem, id=int(operation_system))
         asset_collection = asset_collection.filter(os=os)
-        
+
         return render(request, 'asset/asset/list.html', {
             'os': os,
             'oses': oses,
@@ -22,8 +23,25 @@ def AssetList(request, operation_system=None):
             'oses': oses,
             'asset_collection': asset_collection,
         })
-    
+
+
 # Страница актива
+@login_required
 def AssetDetail(request, id, slug):
     asset = get_object_or_404(Asset, id=id, slug=slug)
     return render(request, 'asset/asset/detail.html', {'asset': asset})
+
+
+# Список тревог
+@login_required
+def AlertList(request, asset=None, alert_type=None):
+    alerts = Alert.objects.all().order_by('-time')
+
+    if asset is not None:
+        alerts = alerts.filter(asset=get_object_or_404(Asset, slug=asset))
+
+    if alert_type is not None:
+        alerts = alerts.filter(type=get_object_or_404(AlertType,
+                                                      pk=alert_type))
+
+    return render(request, 'asset/alert/list.html', {'alerts': alerts})
