14e42f5b8345399279970a0b6b615fa8cc0bba12
Andrey Semakin <and-semakin@ya.ru>
2017-09-15 01:34:04 +0500

    Edit urls.py to comply PEP-8

--- webpanel/asset/urls.py
+++ webpanel/asset/urls.py
@@ -5,9 +5,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.AssetList, name='AssetList'),
-    url(r'^(?P<operation_system>\d+)/$', views.AssetList, name='AssetListByCategory'),
-    url(r'^(?P<slug>[-\w]+)/(?P<id>\d+)/$', views.AssetDetail, name='AssetDetail'),
+    url(r'^(?P<operation_system>\d+)/$',
+        views.AssetList, name='AssetListByCategory'),
+    url(r'^(?P<slug>[-\w]+)/(?P<id>\d+)/$',
+        views.AssetDetail, name='AssetDetail'),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
