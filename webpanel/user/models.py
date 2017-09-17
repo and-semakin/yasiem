43edaabab5af0f3983b09ecbadd9519a892d3d43
Andrey Semakin <and-semakin@ya.ru>
2017-09-17 13:39:50 +0500

    Change user model to use OneToOneField with unique instead of ForeignKey.
    I just follow Django's recommendations.

--- webpanel/user/models.py
+++ webpanel/user/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 # Модель профиля пользователя
 class UserProfile(models.Model):
-    user = models.ForeignKey(User, unique=True)
-    phone =  models.CharField(verbose_name='Телефон', max_length=15, blank=True)
-    position =  models.CharField(verbose_name='Должность', max_length=64, blank=True)
+    user = models.OneToOneField(User)
+    phone = models.CharField(verbose_name='Телефон',
+                             max_length=15, blank=True)
+    position = models.CharField(verbose_name='Должность',
+                                max_length=64, blank=True)
+
 
 User.profile = property(lambda u: UserProfile.objects.get_or_create(user=u)[0])
